d237471dc3046280493e0bec9804c112d679ec08
gmonferrer <gmonferrer@hotmail.com>
2022-05-17 12:30:15 +0200

    Update math.php
    otra modificación en un comentario

--- math.php
+++ math.php
@@ -7,7 +7,7 @@ $radio = 130;
 $perimetro = 2*pi()*$radio;
 // echo $perimetro;
 //fjhkdsfjkdshf
-//mas modificaciones
+//mas modificaciones y otra mas 
 
 
 // echo(min(100, 150, 30, 20, -8, -200));  // -200
