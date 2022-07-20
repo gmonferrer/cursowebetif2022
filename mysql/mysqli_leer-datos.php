e80508e0bda6bbadf973d6a24a18ceb339843f4f
gmonferrer <gmonferrer@hotmail.com>
2022-07-20 17:26:16 +0200

    mas commits

---
+++ mysql/mysqli_leer-datos.php
@@ -0,0 +1,29 @@
+<?php
+// Realizamos una conexion a la base de datos.
+$conexion = new mysqli('localhost', 'root', '', 'heidisql_curso');
+
+// Nos retorna un codigo de error o si todo esta bien regresa 0.
+// echo $conexion->connect_errno;
+
+if ($conexion->connect_errno){
+	die('Lo siento hubo un problema con el servidor');
+} else {
+	//$id = isset($_GET['id']) ? $_GET['id'] : 1; // si descomento esta y la siguiente puedo poner en el navegador ? id=2
+	//$sql = "SELECT * FROM usuarios WHERE id = $id"; 
+	$sql = "SELECT * FROM usuarios";
+	$resultado = $conexion->query($sql);
+	
+	if($resultado->num_rows){
+		echo '<pre>';
+		var_dump($resultado->fetch_assoc()); // aquí me sale el array y solo con el primer valor
+		echo $resultado->fetch_assoc()['nombre'];//con el fetch salta y entonces me da solo el nombre, así entendemos el fetch
+		echo '</pre>';
+
+		while($fila = $resultado->fetch_assoc()){ 
+			echo $fila['id'] . ' - ' . $fila['nombre'] . '<br />';
+		}
+
+	} else {
+		echo 'No hay datos';
+	}
+}
