1f37dfcfb3b87d16d93009eb92e1214530bf6d0b
gmonferrer <gmonferrer@hotmail.com>
2022-09-08 17:27:44 +0200

    jueves

--- php/leer-select/index.view.php
+++ php/leer-select/index.view.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Formulario Contacto</title>
+	<title>Buscar registros</title>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
@@ -10,8 +10,12 @@
 </head>
 <body>
 	<div class="wrap">
+		<h1>Búsqueda de registros</h1>
 		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
 			<p>Selecciona el registro a mostrar</p>
+			<!-- Para varios números -->
+			<input type="number" id="ids" name="ids" min="1" max="100">
+			<!-- la class no hace nada -->
 			<select name="id" id="id" class="form-control">
 				<option value="1">1</option>
 				<option value="2">2</option>
@@ -23,8 +27,11 @@
 				<option value="8">8</option>
 				<option value="9">9</option>
 				<option value="10">10</option>
+				<option value="11">11</option>
+				<option value="12">12</option>
 
 			</select>
+			<!-- lo de la class no hace nada -->
 			<input type="submit" name="submit" class="btn btn-primary" value="Mostrar registro">
 		</form>
 	</div>
--- php/leer-select/marc-index.php
+++ php/leer-select/marc-index.php
@@ -1,16 +1,19 @@
 <?php 
 
 	$errores = '';
-	$enviado = '';
+	$enviado = false;
+
+	//Si hemos hecho clic sobre el boton:
 
 	if (isset($_POST['submit'])) {
+		//guardamos el valor asignado por el usuario en la id
 		$id = $_POST['id'];
 
 		//Error si no hay "id" seleccionada
 		if (empty($id)) {
 			$errores .= 'Por favor selecciona una id <br />';
 		}
-
+// si han dado una id se envía el formulario
 		if(!$errores){
 			$enviado = 'true';
 		}
@@ -18,19 +21,26 @@
 	}
 
 	require 'index.view.php';
+	//a partir del envio del formulario se conecta y envia a la bbdd la id, conectamos con la api
 
 	if ($enviado == 'true'){
 		try {
 			$conexion = new PDO('mysql:host=localhost;dbname=heidisql_curso', 'root', '');
 
 			//Buscar datos del formulario
+			//la variable conexion y el statement podemos poner el nombre que queramos
 			$statement = $conexion->prepare('SELECT * FROM formulario WHERE id = :id');
 			$statement->execute(
 				array(':id'=> $id)
 			);
+			//recibimos otro paquete de información
 
+			//en la siguiente linea es cuando se guarda el resultado de la instrucc sql en la variable resultados
+			//el fetch me lee toda la fila de la consulta
+			//guardamos el registro
 			$resultados = $statement->fetch();
 			
+			//como muestro los datos
 			if($resultados){
 				echo "<p style='text-align:center'> El registro es:</br> ";
 				echo "ID: " . $resultados['id'] . ' - ' . $resultados['nombre']. ' - ' . $resultados['correo'] ;
