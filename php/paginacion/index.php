f3907ccfdae051e0326b6c4f585c4630bd9d1b7f
gmonferrer <gmonferrer@hotmail.com>
2022-09-09 17:50:15 +0200

    cambios

--- php/paginacion/index.php
+++ php/paginacion/index.php
@@ -1,6 +1,6 @@
 <?php 
 
-// Nos conectamos a la base de datos
+// Nos conectamos a la base de datos y si no die para que no nos muestre un montón de errores
 try {
 	$conexion = new PDO('mysql:host=localhost;dbname=curso_paginacion', 'root', '');
 } catch (PDOException $e) {
@@ -9,35 +9,45 @@ try {
 }
 
 // Establecemos el numero de pagina en la que el usuario se encuentra.
-# Esto lo hacemos por el metodo GET, si no hay ningun valor entonces le asignamos la pagina 1.
+# Esto lo hacemos por el metodo GET, si no hay ningun valor entonces le asignamos la pagina 1, si no ya tiene un valor
 $pagina = isset($_GET['pagina']) ? (int)$_GET['pagina'] : 1;
 
-// Establecemos cuantos post por pagina queremos cargar.
+// Establecemos cuantos articulos por pagina queremos cargar.
 $postPorPagina = 5;
 
 // Revisamos desde que articulo vamos a cargar, dependiendo de la pagina en la que se encuentre el usuario.
 # Comprobamos si la pagina en la que esta es mayor a 1, sino entonces cargamos desde el articulo 0.
 # Si la pagina es mayor a 1 entonces hacemos un calculo para saber desde que post cargaremos.
+
+//aqui inicio calcula el primer artículo que muestra en cada página 2*3-3 = 3 (que es el 4 porque la posicion 0 existe)
+
+//$inicio = ($pagina > 1) ? ($pagina * $postPorPagina - $postPorPagina) : 0 ;
+
+//intento hacer que me muestre del artículo más reciente al más antiguo:
 $inicio = ($pagina > 1) ? ($pagina * $postPorPagina - $postPorPagina) : 0 ;
 
-// Preparamos la consulta SQL
+// Preparamos la consulta SQL, articulos es el statement, está conectado con la misma variable del otro fichero view
 $articulos = $conexion->prepare("
 	SELECT SQL_CALC_FOUND_ROWS * FROM articulos
 	LIMIT $inicio, $postPorPagina
-");
+	"); //limitamos desde el inicio a los articulos por pagina
 
 // Ejecutamos la consulta
 $articulos->execute();
 $articulos = $articulos->fetchAll();
 
 // Comprobamos que haya articulos, sino entonces redirigimos a la pagina 1.
+//En el navegador pongo pagina 6 por ejemplo y entonces regresa a la 1
 if (!$articulos) {
 	header('Location: http://localhost/cursowebetif2022/php/paginacion/');
 }
 
 // Calculamos el total de articulos, para despues conocer el numero de paginas de la paginacion.
-$totalArticulos = $conexion->query('SELECT FOUND_ROWS() as total');
+$totalArticulos = $conexion->query('SELECT FOUND_ROWS() as total'); //me guarda el numero de articulos
 $totalArticulos = $totalArticulos->fetch()['total'];
+// es el numero de artículos en una página, en la última por ejemplo serán menos
+//como antes habíamos calculado el total de las ROWS, aunque lo limite,  ahora me las encuntra
+
 
 // Calculamos el numero de paginas que tendra la paginacion.
 # Para esto dividimos el total de articulos entre los post por pagina
--- php/paginacion/index.view.php
+++ php/paginacion/index.view.php
@@ -1,45 +1,57 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="estilos.css">
-	<title>Paginacion</title>
+	<title>Paginación</title>
 </head>
 <body>
 	<div class="contenedor">
 		<h1>Articulos</h1>
-		<section class="articulos">
+
+		<section class="articulos"> 
+			<!-- son los artículos, se dividen con una linea que está en estilos-->
 			<ul>
+				<!-- la variable artículos es el array, el foreach recorre la array y va poniendo 1. artículo1 -->
 				<?php foreach ($articulos as $articulo): ?>
 					<li><?php echo $articulo['id'] . '.- ' . $articulo['articulo']; ?></li>
 				<?php endforeach; ?>
 			</ul>
 		</section>
 
-		<div class="paginacion">
+		<div class="paginacion"> 
 			<ul>
+			<!-- es lo de abajo -->
 				<!-- Establecemos cuando el boton de "Anterior" estara desabilitado -->
 				<?php if($pagina == 1): ?>
+					<!-- &laquo muestra lo de la flecha hacia la izquierda -->
 					<li class="disabled">&laquo;</li>
+					<!-- cuando el disabled no le pongo el enlace, lo otro solo es el estilo del cursor -->
 				<?php else: ?>
 					<li><a href="?pagina=<?php echo $pagina - 1 ?>">&laquo;</a></li>
 				<?php endif; ?>
 
-				<!-- Ejecutamos un ciclo para mostrar las paginas -->
+
+				<!-- Ejecutamos un ciclo para mostrar los númros de las paginas, no las flechas -->
 				<?php 
+				// la variable numeropaginas es el total de paginas
+				//la class active es naranja, pero con el hover se vuelve azul
 					for($i = 1; $i <= $numeroPaginas; $i++){
 						if ($pagina === $i) {
+							// simplificando a tope echo<li>$i</li>
 							echo "<li class='active'><a href='?pagina=$i'>$i</a></li>";
 						} else {
+							// sale en negro
 							echo "<li><a href='?pagina=$i'>$i</a></li>";
 						}
 					}
 				 ?>
-
+				<!-- La variable página es donde nos encontramos ahora -->
 				<!-- Establecemos cuando el boton de "Siguiente" estara desabilitado -->
 				<?php if($pagina == $numeroPaginas): ?>
+				<!-- &raquo muestra lo de la flecha hacia la derecha -->	
 					<li class="disabled">&raquo;</li>
 				<?php else: ?>
 					<li><a href="?pagina=<?php echo $pagina + 1 ?>">&raquo;</a></li>
